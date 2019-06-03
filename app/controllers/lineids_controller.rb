a5e346175f8950bfa9968bd25ef5fe9de5dc17af
Naoya Sumii <awapon708@gmail.com>
2019-06-03 22:52:17 +0900

    add count

--- app/controllers/lineids_controller.rb
+++ app/controllers/lineids_controller.rb
@@ -1,11 +1,13 @@
 class LineidsController < ApplicationController
   def before_index
     @lineids = Lineid.where(status: 0).page(params[:page])
+    @count_lineids = Lineid.where(status: 0).count
     @lineid=Lineid.new
   end
 
   def after_index
-    @lineids = Lineid.where(status: 1)
+    @lineids = Lineid.where(status: 1).page(params[:page])
+    @count_lineids = Lineid.where(status: 1).count
   end
 
   def status
